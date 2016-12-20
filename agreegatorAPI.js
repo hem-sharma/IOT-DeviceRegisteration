8e61085c5404c039946cb890bafb6f2514a4d428
kaushik-hemant <kaushik.hemant@live.com>
2016-12-20 23:00:58 +0530

    minor updates

--- agreegatorAPI.js
+++ agreegatorAPI.js
@@ -1,16 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
-//Getting config for Api
 var config = require(__dirname + '/APIConfig.json');
 
-//getting port for server process
 var processPort = config.port;
 
-//Reciever process file location
 var file = config.reciever;
 
-//port for getting data
+
 var SerialPort = require('serialport');
 
 //baud rate can be changeable from config file
@@ -19,20 +16,35 @@ var port = new SerialPort.SerialPort(file, {
   parser: SerialPort.parsers.readline('\r\n')
 });
 
-//GPS for getting data
+//custom module for getting gps data
 var GPS = require(__dirname + '/gps.js');
+
 var gps = new GPS;
 
 
 gps.on('GGA', function (data) {
-  //TODO: calling API to store the data in VA db
   console.log('data recieved', data);
-  //io.emit('position', data);
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/AssignAgreegator', function (req, res) {
+  //TODO: update agreegator Id consumed by sending GPS data
+});
+
+app.get('/AddWifiCredentials', function (req, res) {
+  //TODO: update wifi and password value usign updateConfigWithAgreegatorId and then change the device wifi file
+});
+
+function updateConfigWithAgreegatorId(obj, val) {
+
+}
+
+function appendWiFiConfigCreds(ssid, password) {
+
+}
 
 http.listen(processPort, function () {
   console.log('listening on *:' + processPort);
